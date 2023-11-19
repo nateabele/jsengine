f9d9721b0b70131cce013f4cd882d3d93b4e7ab1
Nate Abele <nate.abele@gmail.com>
2023-11-18 23:09:57 -0600

    New thread-safe version with channels
    Still needs work

---
+++ native/jsengine/src/engine.rs
@@ -0,0 +1,133 @@
+use crate::conv::{serde_v8_error_to_json, anyhow_error_to_json};
+
+use deno_core::{anyhow, v8, op2, serde_v8, Extension, FastString, JsRuntime, ModuleCode, Op, RuntimeOptions};
+use deno_core::serde_json::Value;
+use deno_core::error::AnyError;
+use tokio;
+
+pub(crate) type JsResult = Result<Value, Value>;
+
+pub enum Request {
+    Load(Vec<String>),
+    Run(String),
+    Call(String, Vec<Value>),
+}
+
+pub(crate) struct Engine {
+    runtime: JsRuntime
+}
+
+impl Engine {
+
+    pub fn new() -> Self {
+        let mut new_engine = Engine {
+            runtime: JsRuntime::new(RuntimeOptions {
+                extensions: vec![Extension {
+                    name: "core:apis",
+                    ops: std::borrow::Cow::Borrowed(&[op_set_timeout::DECL]),
+                    ..Default::default()
+                }],
+                ..Default::default()
+            })
+        };
+        new_engine.runtime.execute_script_static("[core:runtime]", include_str!("./runtime.js")).unwrap();
+
+        new_engine
+    }
+
+    async fn run(&mut self, code: &String) -> JsResult {
+        let result = eval_raw(&mut self.runtime, code).await.map(|val| {
+            let scope = &mut self.runtime.handle_scope();
+            let local = v8::Local::new(scope, val);
+            serde_v8::from_v8::<Value>(scope, local)
+        });
+        
+        match result {
+            Ok(Ok(value)) => Ok(value),
+            Ok(Err(err)) => Err(serde_v8_error_to_json(&err)),
+            Err(err) => Err(anyhow_error_to_json(&err)),
+        }
+    }
+      
+    async fn load(&mut self, js_files: &Vec<String>) -> JsResult {
+        for file_path in js_files {
+            // @TODO Return errors as Err
+            let contents = std::fs::read_to_string(file_path).expect("File read error");
+            let _ = self.run(&contents).await;
+        }
+        Ok(Value::Null)
+    }
+
+    async fn call(&mut self, fn_name: &String, args: &Vec<Value>) -> JsResult {
+        call_internal(&mut self.runtime, &fn_name, &args).await
+    }
+
+    pub async fn handle(&mut self, req: &Request) -> JsResult {
+        match req {
+            Request::Load(files) => self.load(files).await,
+            Request::Run(code) => self.run(code).await,
+            Request::Call(fn_name, args) => self.call(&fn_name, &args).await,
+        }
+    }
+}
+
+pub async fn call_internal<'a>(js_runtime: &mut JsRuntime, fn_name: &String, args: &Vec<Value>) -> JsResult {
+
+  let call_result = {
+      let scope = &mut js_runtime.handle_scope();
+      let context = scope.get_current_context();
+      let global = context.global(scope);
+
+      let fn_key = v8::String::new(scope, fn_name).unwrap();
+      let func = global.get(scope, fn_key.into()).unwrap();
+      let func = v8::Local::<v8::Function>::try_from(func);
+
+      match func {
+          Ok(func) => {
+              let v8_args: Vec<v8::Local<v8::Value>> = args
+                  .into_iter()
+                  .map(|arg| serde_v8::to_v8(scope, arg).unwrap())
+                  .collect();
+  
+              // @TODO: Remove unwrap, handle safely
+              let local = func.call(scope, global.into(), &v8_args).unwrap();
+              Ok(v8::Global::new(scope, local))
+          },
+          Err(_) => Err(Value::String(format!("{} is not a callable function", fn_name)))
+      }
+  };
+
+  match call_result {
+      Ok(result) => {
+          let encoded = js_runtime.resolve_value(result).await.map(|result| {
+              let scope = &mut js_runtime.handle_scope();
+              let local = v8::Local::new(scope, result);
+              serde_v8::from_v8::<Value>(scope, local)
+          });
+      
+          match encoded {
+              Ok(Ok(value)) => Ok(value),
+              Ok(Err(err)) => Err(serde_v8_error_to_json(&err)),
+              Err(err) => Err(anyhow_error_to_json(&err)),
+          }
+      },
+      Err(err) => Err(err)
+  }
+}
+
+
+async fn eval_raw(js_runtime: &mut JsRuntime, code: &String) -> Result<v8::Global<v8::Value>, anyhow::Error> {
+  let module: ModuleCode = FastString::from(code.to_owned());
+  let result = js_runtime.execute_script("[core]", module);
+
+  match result {
+      Ok(value) => js_runtime.resolve_value(value).await,
+      Err(err) => Err(err)
+  }
+}
+
+#[op2(async)]
+async fn op_set_timeout(#[serde] delay: u64) -> Result<(), AnyError> {
+  tokio::time::sleep(std::time::Duration::from_millis(delay)).await;
+  Ok(())
+}
--- native/jsengine/src/lib.rs
+++ native/jsengine/src/lib.rs
@@ -1,170 +1,108 @@
-pub mod atoms;
-pub mod conv;
+#[allow(unused_imports)]
+
+mod atoms;
+mod conv;
 mod error;
+mod engine;
 
-use crate::conv::{json_to_term, term_to_json, serde_v8_error_to_json, anyhow_error_to_json};
+use crate::conv::{json_to_term, term_to_json};
+use crate::engine::{Engine, JsResult, Request};
+use crate::engine::Request::{Load, Call, Run};
 
-use deno_core::{anyhow, v8, op2, serde_v8, Extension, FastString, JsRuntime, ModuleCode, Op, RuntimeOptions};
 use deno_core::serde_json::Value;
-use deno_core::error::AnyError;
-use std::sync::Mutex;
-use lazy_static::lazy_static;
 use rustler::{Encoder, Env, NifResult, Term};
+
+use std::thread;
+use std::sync::{Arc, Mutex};
+use std::sync::mpsc::{channel, Sender};
+use once_cell::sync::Lazy;
 use tokio;
 
 rustler::init!("Elixir.JSEngine", [load, run, call], load = init);
 
-type JsResult = Result<Value, Value>;
+static GLOBAL_CHANNEL: Lazy<Arc<Mutex<Sender<(Request, Sender<JsResult>)>>>> = Lazy::new(|| {
+    let (sender, receiver) = channel::<(Request, Sender<JsResult>)>();
+    let sender = Arc::new(Mutex::new(sender));
+    let runtime = tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap();
 
-struct Engine {
-    runtime: JsRuntime
-}
+    /* Spawn the master thread */
+    thread::spawn(move || {
+        let mut engine = Engine::new();
 
-unsafe impl Sync for Engine {}
-unsafe impl Send for Engine {}
-
-lazy_static! {
-    static ref REGISTRY: Mutex<Engine> = Mutex::new(Engine {
-        runtime: JsRuntime::new(RuntimeOptions {
-            extensions: vec![Extension {
-                name: "core:apis",
-                ops: std::borrow::Cow::Borrowed(&[op_set_timeout::DECL]),
-                ..Default::default()
-            }],
-            ..Default::default()
-        })
+        for (request, response_sender) in receiver {
+            let async_result = engine.handle(&request);
+            let result = runtime.block_on(async_result);
+            let _ = response_sender.send(result);
+        }
     });
-}
+
+    sender
+});
+
 
 fn init(_env: Env, _term: rustler::Term) -> bool {
-    let mut engine = REGISTRY.lock().unwrap();
-    engine.runtime.execute_script_static("[core:runtime]", include_str!("./runtime.js")).unwrap();
+    // engine.runtime.execute_script_static("[core:runtime]", include_str!("./runtime.js")).unwrap();
     true
 }
 
 #[rustler::nif(schedule = "DirtyCpu")]
 fn load<'a>(env: Env<'a>, js_files: Vec<String>) -> NifResult<Term<'a>> {
-    let runtime = tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap();
+    let (sender, receiver) = channel::<JsResult>();
 
-    match REGISTRY.lock() {
-        Ok(mut engine) => {
-            for file_path in js_files {
-                let contents = std::fs::read_to_string(file_path).expect("File read error");
-                let _ = runtime.block_on(eval(&mut engine.runtime, &contents));
-            }
-            Ok(atoms::ok().encode(env))
-        },
-        Err(_poison_error) => {
-            Err(rustler::Error::Atom("mutex_poisoned"))
-        }
-    }
+    // @TODO Handle unwraps
+    let global_sender = GLOBAL_CHANNEL.lock().unwrap();
+    global_sender.send((Load(js_files), sender)).unwrap();
+
+    let result = receiver.recv().unwrap();
+
+    // match REGISTRY.lock() {
+    //     Ok(mut engine) => {
+    //         Ok(atoms::ok().encode(env))
+    //     },
+    //     Err(_poison_error) => {
+    //         Err(rustler::Error::Atom("mutex_poisoned"))
+    //     }
+    // }
+    Ok(match result {
+        Ok(value) => (atoms::ok(), json_to_term(env, &value)).encode(env),
+        Err(err) => (atoms::error(), json_to_term(env, &err)).encode(env)
+    })
+    
 }
 
 #[rustler::nif]
 fn run<'a>(env: Env<'a>, code: String) -> NifResult<Term<'a>> {
-    let runtime = tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap();
-    let result = REGISTRY.lock().map(|mut engine| runtime.block_on(eval(&mut engine.runtime, &code)));
+    let (sender, receiver) = channel::<JsResult>();
 
-    match result {
-        Ok(Ok(val)) => Ok((atoms::ok(), json_to_term(env, &val)).encode(env)),
-        Ok(Err(err)) => Ok((atoms::error(), json_to_term(env, &err)).encode(env)),
-        Err(_poison_error) => Err(rustler::Error::Atom("mutex_poisoned"))
-    }
+    // @TODO Handle unwraps
+    let global_sender = GLOBAL_CHANNEL.lock().unwrap();
+    global_sender.send((Run(code), sender)).unwrap();
+
+    let result = receiver.recv().unwrap();
+
+    Ok(match result {
+        Ok(val) => (atoms::ok(), json_to_term(env, &val)).encode(env),
+        Err(err) => (atoms::error(), json_to_term(env, &err)).encode(env),
+        // Err(_poison_error) => Err(rustler::Error::Atom("mutex_poisoned"))
+    })
 }
 
 #[rustler::nif]
 fn call<'a>(env: Env<'a>, fn_name: String, args: Vec<Term<'a>>) -> NifResult<Term<'a>> {
-    let runtime = tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap();
-    let result = REGISTRY.lock().map(|mut engine| {
-        runtime.block_on(call_internal(
-            &mut engine.runtime,
-            &fn_name,
-            &args.into_iter().map(|arg| term_to_json(env, arg).unwrap()).collect()
-        ))
-    });
+    let json_args: Vec<Value> = args.into_iter().map(|arg| term_to_json(env, arg).unwrap()).collect();
+    let (sender, receiver) = channel::<JsResult>();
 
-    match result {
-        Ok(Ok(val)) => Ok((atoms::ok(), json_to_term(env, &val)).encode(env)),
-        Ok(Err(err)) => Ok((atoms::error(), json_to_term(env, &err)).encode(env)),
-        Err(_poison_error) => Err(rustler::Error::Atom("mutex_poisoned"))
-    }
-}
+    // @TODO Handle unwraps
+    let global_sender = GLOBAL_CHANNEL.lock().unwrap();
+    global_sender.send((Call(fn_name, json_args.into()), sender)).unwrap();
 
-async fn call_internal<'a>(js_runtime: &mut JsRuntime, fn_name: &String, args: &Vec<Value>) -> JsResult {
+    let _result = receiver.recv().unwrap();
 
-    let call_result = {
-        let scope = &mut js_runtime.handle_scope();
-        let context = scope.get_current_context();
-        let global = context.global(scope);
+    // match result {
+    //     Ok(Ok(val)) => Ok((atoms::ok(), json_to_term(env, &val)).encode(env)),
+    //     Ok(Err(err)) => Ok((atoms::error(), json_to_term(env, &err)).encode(env)),
+    //     Err(_poison_error) => Err(rustler::Error::Atom("mutex_poisoned"))
+    // }
 
-        let fn_key = v8::String::new(scope, fn_name).unwrap();
-        let func = global.get(scope, fn_key.into()).unwrap();
-        let func = v8::Local::<v8::Function>::try_from(func);
-
-        match func {
-            Ok(func) => {
-                let v8_args: Vec<v8::Local<v8::Value>> = args
-                    .into_iter()
-                    .map(|arg| serde_v8::to_v8(scope, arg).unwrap())
-                    .collect();
-    
-                // @TODO: Remove unwrap, handle safely
-                let local = func.call(scope, global.into(), &v8_args).unwrap();
-                Ok(v8::Global::new(scope, local))
-            },
-            Err(_) => Err(Value::String(format!("{} is not a callable function", fn_name)))
-        }
-    };
-
-    match call_result {
-        Ok(result) => {
-            let encoded = js_runtime.resolve_value(result).await.map(|result| {
-                let scope = &mut js_runtime.handle_scope();
-                let local = v8::Local::new(scope, result);
-                serde_v8::from_v8::<Value>(scope, local)
-            });
-        
-            match encoded {
-                Ok(Ok(value)) => Ok(value),
-                Ok(Err(err)) => Err(serde_v8_error_to_json(&err)),
-                Err(err) => Err(anyhow_error_to_json(&err)),
-            }
-        },
-        Err(err) => Err(err)
-    }
-}
-
-async fn eval(context: &mut JsRuntime, code: &String) -> JsResult {
-    let result = eval_raw(context, code).await.map(|val| {
-        let scope = &mut context.handle_scope();
-        let local = v8::Local::new(scope, val);
-        serde_v8::from_v8::<Value>(scope, local)
-    });
-
-    match result {
-        Ok(Ok(value)) => Ok(value),
-        Ok(Err(err)) => Err(serde_v8_error_to_json(&err)),
-        Err(err) => Err(anyhow_error_to_json(&err)),
-    }
-}
-
-async fn eval_raw(js_runtime: &mut JsRuntime, code: &String) -> Result<v8::Global<v8::Value>, anyhow::Error> {
-    let module: ModuleCode = FastString::from(code.to_owned());
-    let result = js_runtime.execute_script("[core]", module);
-
-    match result {
-        Ok(value) => {
-            let resolved = js_runtime.resolve_value(value);
-            resolved.await
-        },
-        Err(err) => {
-            Err(err)
-        }
-    }
-}
-
-#[op2(async)]
-async fn op_set_timeout(#[serde] delay: u64) -> Result<(), AnyError> {
-    tokio::time::sleep(std::time::Duration::from_millis(delay)).await;
-    Ok(())
+    Ok(atoms::ok().encode(env))
 }
